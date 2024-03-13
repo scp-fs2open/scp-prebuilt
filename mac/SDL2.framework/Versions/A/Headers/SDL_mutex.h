77372cd9b92c433f6b912779717915f33026f76f
Taylor Richards <taylor@notimaginative.com>
2024-03-13 18:56:58 -0400

    update lib setup for all platforms...
    - Update SDL2 to version 2.26.5:
    
      Adds a prebuilt version of SDL2 for all platforms, allowing the SDL2 libs to
      be removed from the FSO source tree. Version 2.26.5 is new enough to contain
      Windows arm64 support and better macOS support but old enough to have been
      stabilized with multiple bug fix releases. It also contains improved joystick
      and gamepad support across all platforms.
    
    - Add freetype, openal, sdl2 libs to Linux:
    
      Previously we've just let the system libs be used, however some newer Linux
      distros don't include them directly. We also don't know what versions or
      features are available which can cause support issues. All three libs are
      built on Ubuntu 18.04. The openal and sdl2 libs are built per the docs
      to allow for the maximum feature set without additional dependencies. The
      freetype lib is built the same as the macOS version and statically linked.
    
      Fixes #6017
    
    - Add support for Windows arm64:
    
      Where possible the libs were built with the same versions and options as those
      used for the Linux and macOS builds. SDL2 was built with the default options
      for Windows. The ffmpeg libs were built with llvm-mingw using ucrt and should
      support Windows 10 and above. However arm64 support generally means
      Windows 11 or above, so that is the target.
    
    - Make sure macOS libs/frameworks are signed
    
      Use an ad-hoc signature for libs and frameworks on macOS. We do this with a
      build step in CI but that just covers those builds, so this takes care of it
      upstream for everyone else.
    
      Fixes #2875

--- mac/SDL2.framework/Versions/A/Headers/SDL_mutex.h
+++ mac/SDL2.framework/Versions/A/Headers/SDL_mutex.h
@@ -1,6 +1,6 @@
 /*
   Simple DirectMedia Layer
-  Copyright (C) 1997-2022 Sam Lantinga <slouken@libsdl.org>
+  Copyright (C) 1997-2023 Sam Lantinga <slouken@libsdl.org>
 
   This software is provided 'as-is', without any express or implied
   warranty.  In no event will the authors be held liable for any damages
@@ -28,10 +28,10 @@
  *  Functions to provide thread synchronization primitives.
  */
 
-#include "SDL_stdinc.h"
-#include "SDL_error.h"
+#include <SDL2/SDL_stdinc.h>
+#include <SDL2/SDL_error.h>
 
-#include "begin_code.h"
+#include <SDL2/begin_code.h>
 /* Set up for C function definitions, even when using C++ */
 #ifdef __cplusplus
 extern "C" {
@@ -464,7 +464,7 @@ extern DECLSPEC int SDLCALL SDL_CondWaitTimeout(SDL_cond * cond,
 #ifdef __cplusplus
 }
 #endif
-#include "close_code.h"
+#include <SDL2/close_code.h>
 
 #endif /* SDL_mutex_h_ */
 
